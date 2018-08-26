a846c72e085406bd6f90524d20f91f9f8052fd6a
Luyanda MNCUBE <lmncube@e4r21p1.wethinkcode.co.za>
2018-08-26 11:56:28 +0200

    Fixed dump.c

--- src/dump.c
+++ src/dump.c
@@ -6,7 +6,7 @@
 /*   By: lmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/24 11:10:44 by lmncube           #+#    #+#             */
-/*   Updated: 2018/08/24 16:55:30 by lmncube          ###   ########.fr       */
+/*   Updated: 2018/08/26 11:56:04 by lmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	dump(t_stack *a, t_stack *b	)
 	{
 		a->top ? ft_putnbr(a->s[biggest]) : ft_putstr(" ");
 		ft_putstr("\t\t");
-		b->top ? ft_putnbr(b->s[biggest]) : ft_putstr(" ");
+		b->top > biggest ? ft_putnbr(b->s[biggest]) : ft_putstr(" ");
 		ft_putstr("\n");
 		biggest--;
 	}
