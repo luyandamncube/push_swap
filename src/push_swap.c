82ae871e2c9572f8eb7b15c5f2e1fbf2caa12921
Luyanda MNCUBE <lmncube@e4r21p1.wethinkcode.co.za>
2018-08-27 16:13:14 +0200

    Re-fixed stacking

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lmncube <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/24 11:23:06 by lmncube           #+#    #+#             */
-/*   Updated: 2018/08/27 11:51:42 by lmncube          ###   ########.fr       */
+/*   Updated: 2018/08/27 15:02:17 by lmncube          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,21 @@ int	main(int argc, char **argv)
 	int		k;
 
 	size = argc - 1;
-	k = 0;
+	k = 1;
 	initialize(&stack_a, 'a');	
 	initialize(&stack_b, 'b');	
 	if (argc > 1)
 	{
 		while (argc > 1)
 		{
-			if (ft_isnum(argv[size]))
-				push(ft_atoi(argv[size]), stack_a);
+			if (ft_isnum(argv[k]))
+				push(ft_atoi(argv[k]), stack_a);
 			else
 			{
 				ft_putstr("Error\n");
 				exit (1);
 			}
-			size--;
+			k++;
 			argc--;
 		}
 		if (check_duplicates(stack_a))
@@ -42,7 +42,8 @@ int	main(int argc, char **argv)
 		else
 		{
 			//algo_1(stack_a, stack_b);
-			printf("swap %d\n", push_to(stack_b, stack_a));
+			printf("rot a %d\n", rot(stack_a));
+			printf("elem 1 %d\n", stack_a->s[0]);
 			printf("sorted %d\n", is_sorted(stack_a->s, stack_a->top));
 			dump(stack_a, stack_b);
 		}
